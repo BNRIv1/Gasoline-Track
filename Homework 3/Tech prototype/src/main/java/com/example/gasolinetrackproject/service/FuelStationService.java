62573528381be0c583088418c46ced24f837be01
BNRI <bojan.ristevskI@yahoo.com>
2023-02-06 16:11:29 +0100

    Adding description to methods in FuelStationService

--- Homework 3/Tech prototype/src/main/java/com/example/gasolinetrackproject/service/FuelStationService.java
+++ Homework 3/Tech prototype/src/main/java/com/example/gasolinetrackproject/service/FuelStationService.java
@@ -9,10 +9,25 @@ public interface FuelStationService {
 
     Optional<FuelStation> save(String name, String city, String coordinate_x, String coordinate_y);
     List<FuelStation> listAll();
+
+    /**
+     *
+     * @return list of all cities in which there is a gas station
+     */
     List<String> findDistinctByCity();
 
+    /**
+     *
+     * @return list of unique gas station names
+     */
     List<String> findDistinctByName();
 
+    /**
+     *
+     * @param name to be filtered by
+     * @param city to be filtered by
+     * @return list of all stations whose name equals param name and city equals param city
+     */
     List<FuelStation> filterStations(String name, String city);
 
     Optional<FuelStation> findById(Long id);
