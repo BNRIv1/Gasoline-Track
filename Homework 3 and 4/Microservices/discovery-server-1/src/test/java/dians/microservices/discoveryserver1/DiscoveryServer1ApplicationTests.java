e1a003dac16be8d3664b9ce8bf793c8d64020849
chochevskajovana <jovanachochevska5@gmail.com>
2023-02-12 15:58:56 +0100

    Adding microservices

---
+++ Homework 3 and 4/Microservices/discovery-server-1/src/test/java/dians/microservices/discoveryserver1/DiscoveryServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dians.microservices.discoveryserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Homework 3 and 4/Microservices/stations-list-service/src/main/java/dians/microservices/stationslistservice/models/AllGasStations.java
@@ -0,0 +1,24 @@
+package dians.microservices.stationslistservice.models;
+
+import java.util.List;
+
+public class AllGasStations {
+
+    private List<GasStation> gasStations;
+
+    public List<GasStation> getGasStations() {
+        return gasStations;
+    }
+
+    public void setGasStations(List<GasStation> gasStations) {
+        this.gasStations = gasStations;
+    }
+
+    public AllGasStations(){
+
+    }
+
+    public AllGasStations(List<GasStation> gasStations) {
+        this.gasStations = gasStations;
+    }
+}
